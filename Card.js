2e69e89662c220f62dd0d7ba48ab3c2fff1df92d
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-12 11:44:19 -0500

    Card.js is a legitimate component now

--- Card.js
+++ Card.js
@@ -1,20 +1,23 @@
-const title = '50 Ways To Eat Your Lover';
-const name = 'Paul Simon of the Lambs';
-const saleOn = true;
-
-const markup = <div className="row">
-                    <div className="col s2">
-                        <div className="card hoverable small">
-                            <div className="card-image">
-                                <img src="http://lorempixel.com/400/400/food/" />
-                            </div>
-                            <div className="card-content">
-                                <p>{title}</p>
-                                <p>{name}</p>
-                            </div>
-                            <div className="card-action">
-                                <a href="#">${saleOn ? 9.99 : 59.99}</a>
-                            </div>
+//EVERY REACT COMPONENT MUST RETURN A SINGLE DOM ELEMENT
+function Card(){
+    const title = '50 Ways To Eat Your Lover';
+    const name = 'Paul Simon of the Lambs';
+    const saleOn = true;
+    
+    return  <div className="row">
+                <div className="col s2">
+                    <div className="card hoverable small">
+                        <div className="card-image">
+                            <img src="http://lorempixel.com/400/400/food/" />
                         </div>
-                    </div>    
-                </div>;
+                        <div className="card-content">
+                            <p>{title}</p>
+                            <p>{name}</p>
+                        </div>
+                        <div className="card-action">
+                            <a href="#">${saleOn ? 9.99 : 59.99}</a>
+                        </div>
+                    </div>
+                </div>    
+            </div>;
+}
