ffd1dc5df2520fe9746793e1582b39a96eee7c34
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-12 14:29:12 -0500

    Map() Solution

---
+++ Cardalt.js
@@ -0,0 +1,22 @@
+//EVERY REACT COMPONENT MUST RETURN A SINGLE DOM ELEMENT
+function Card(props){
+    const title = props.data.course;
+    const name = props.data.instructor;
+    const image = props.data.image;
+    const saleOn = true;
+    
+    return  <div className="col s6 m4 l3">
+                <div className="card hoverable small">
+                    <div className="card-image">
+                        <img src={image} />
+                    </div>
+                    <div className="card-content">
+                        <p>{title}</p>
+                        <p>{name}</p>
+                    </div>
+                    <div className="card-action">
+                        <a href="#">${saleOn ? 9.99 : 59.99}</a>
+                    </div>
+                </div>
+            </div>;
+}
