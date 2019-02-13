b33aed63e4ebbe1476e717aa5f41dbf4a4abe160
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-13 11:36:35 -0500

    Fixed issue with this.coin array

--- 13-02-19/coinflip/coinFlip.js
+++ 13-02-19/coinflip/coinFlip.js
@@ -3,9 +3,9 @@ class CoinFlip extends React.Component{
         //This is a child class. It's a child of React.Component, so we need to use super to get all of React.Component's cool stuff!
         super(props);
         //React doesn't care about heads or tails... at all!
-        this.heads = 'buffalo-nickel-heads.jpg';
-        this.tails = 'buffalo-nickel-tails.png';
-        this.coin = [heads,tails];
+        this.heads = 'images/buffalo-nickel-heads.jpg';
+        this.tails = 'images/buffalo-nickel-tails.png';
+        this.coin = [this.heads,this.tails];
         this.state = {
             //React does care about state... a whole lot!
             image:this.coin[0]
@@ -15,6 +15,11 @@ class CoinFlip extends React.Component{
     //local class method
     //EVERY class component must have a render. Without one your app will BREAK!
     render(){
-
+        //render() always runs after the constructor
+        return (
+            <div className="coin-flip">
+                <img src={this.state.image} />
+            </div>
+        );
     }
 }
