f7fd9c78dd581126353eafdbd8644c312b5c7a6d
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-13 14:29:03 -0500

    Beginning to add API

---
+++ 13-02-19/movieReact/movie.js
@@ -0,0 +1,24 @@
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            moviesToShow: []
+        }
+    }
+
+    //React calls this, but we don't
+    componentDidMount(){
+        const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5";
+        console.log("MOUNTED")
+        this.setState({
+            moviesToShow: [1]
+        })
+    }
+
+    render(){
+        console.log("RENDERED")
+        return(
+            <h1>BARK</h1>
+        );
+    };
+}
