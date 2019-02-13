42313ca0dc638751741a1b0c30e8429634e01277
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-13 14:33:56 -0500

    Fetching JSON from API

--- 13-02-19/movieReact/movie.js
+++ 13-02-19/movieReact/movie.js
@@ -9,6 +9,14 @@ class App extends React.Component{
     //React calls this, but we don't
     componentDidMount(){
         const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=fec8b5ab27b292a68294261bb21b04a5";
+        fetch(url)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(myJson) {
+          console.log(myJson);
+        });
+
         console.log("MOUNTED")
         this.setState({
             moviesToShow: [1]
