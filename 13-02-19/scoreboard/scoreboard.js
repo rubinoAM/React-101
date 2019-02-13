739a78076346bd2bfcc560190bfcdff4385d1f1e
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-13 09:51:19 -0500

    Set up new server

---
+++ 13-02-19/scoreboard/scoreboard.js
@@ -0,0 +1,11 @@
+function Application(){
+
+}
+
+const renderWhat = <h1>Sanity Check</h1>;
+const renderWhere = document.getElementById('root');
+
+ReactDOM.render(
+    renderWhat,
+    renderWhere
+);
