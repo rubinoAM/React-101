46a360bc6fbac50867ec40fa40d426d8fdc2b9f3
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-13 14:06:32 -0500

    Refactoring comment.js

--- 13-02-19/extract/comment.js
+++ 13-02-19/extract/comment.js
@@ -28,33 +28,95 @@ var data = [
         ]
     }
 ]
- 
-class User{}
 
-function Comment(props) {
-    return (
-        <div className="Comment">
-        <div className="UserInfo">
-        <img className="Avatar"
-        src={props.author.avatarUrl}
-        alt={props.author.name}
-        />
-        <div className="UserInfo-name">
-        {props.author.name}
-        </div>
-        </div>
-        <div className="Comment-body">
-        <h1>{props.commentHeading}</h1>
-        <div className="Comment-text">{props.text}</div>
-        <div className="Comment-date">
-        {formatDate(props.date)}
-        </div>
-        </div>
-        <div className="UserBadges">
-        <div className="badge">{props.userBadge[0]}</div>
-        <div className="badge">{props.userBadge[1]}</div>
-        <div className="badge">{props.userBadge[2]}</div>
-        </div>
-        </div>
+class Avatar extends React.Component{
+    constructor(props){
+        super(props);
+        this.name = props.name;
+        this.avatar = props.author.avatarUrl;
+    }
+
+    render(){
+        return(
+            <img className="Avatar" src={this.avatar} alt={this.name} />
+        );
+    }
+}
+ 
+class UserInfo extends React.Component{
+    constructor(props){
+        super(props);
+        this.name = props.author.name;
+    }
+
+    render(){
+        return(
+            <div className="UserInfo">
+                <Avatar src={props.author.avatarUrl} alt={this.name} />
+                <div className="UserInfo-name">
+                    {this.name}
+                </div>
+            </div>
+        );
+    }
+}
+
+class CommentBody extends React.Component{
+    constructor(props){
+        super(props);
+        this.commentHeading = props.commentHeading;
+        this.text = props.text;
+        this.date = props.date;
+    }
+
+    formatDate(date){
+        
+    }
+
+    render(){
+        return(
+            <div className="Comment-body">
+                <h1>{this.commentHeading}</h1>
+                <div className="Comment-text">
+                    {this.text}
+                </div>
+                <div className="Comment-date">
+                    {formatDate(this.date)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Badges extends React.Component{
+    constructor(props){
+        super(props);
+        this.userBadge = props.userBadge;
+    }
+
+    render(){
+        return(
+            <div className="UserBadges">
+                {this.userBadge.forEach((badge)=>{
+                    <div className="badge">
+                        {badge}
+                    </div>
+                })}
+            </div>
+        );
+    }
+}
+
+function Comment(props){
+    this.comments = props.map();
+
+    return(
+        this.comments.forEach(()=>{
+            <div className="Comment">
+                <UserInfo />
+                <CommentBody />
+                <Badges userBadge={props.userBadge}/>
+            </div>
+        })
     );
 }
