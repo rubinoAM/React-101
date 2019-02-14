f4e18e7225a6ff74f79a9f13396c7398e82a927d
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-14 15:22:02 -0500

    Fixed issue with className

--- 14-02-19/movie-cra/src/Navbar.js
+++ 14-02-19/movie-cra/src/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends Component{
             <nav>
                 <div className="nav-wrapper">
                     <a className="brand-logo center">MovieApp</a>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li><a href="#">Home</a></li>
                         <li><a href="#">About</a></li>
                         <li><a href="#">Contact</a></li>
