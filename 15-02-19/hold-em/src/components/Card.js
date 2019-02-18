0623ee35ebaafaa0052b9fa449d496da0e4d5d01
rubinoAM <parkcathedralgrace@gmail.com>
2019-02-18 09:46:29 -0500

    Reorganized images

--- 15-02-19/hold-em/src/components/Card.js
+++ 15-02-19/hold-em/src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Card(props){ //Shows a card
-	const theCard = `/cards/${props.card}.png`;
+	const theCard = `/images/cards/${props.card}.png`;
 	return(
 		<div className="col-sm-2">
 			<img src={theCard} className="card" alt=''/>
